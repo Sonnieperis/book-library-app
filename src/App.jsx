dbfadd6b381712a57e69808cee62a8b7d53628fb
Muthoni(sonnieperis) <sonnieperis17@gmail.com>
2025-08-30 19:34:21 +0300

    installed the packages

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen grid place-items-center bg-gray-50">
+      <h1 className="text-3xl font-bold underline text-blue-600">
+      Book Library
+      </h1>
+    </div>
+  );
+}
