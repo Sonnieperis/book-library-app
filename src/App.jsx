949b7a023bf9b44a0cd5ff97d3895ab95a5620a2
Muthoni(sonnieperis) <sonnieperis17@gmail.com>
2025-09-09 19:34:26 +0300

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-// src/App.jsx
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import BookDetails from "./pages/BookDetails";
@@ -7,14 +5,14 @@ import NotFound from "./pages/NotFound";
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-orange-900 text-white">
-      <Router>
+    <Router>
+      <div className="min-h-screen bg-orange-900 text-white">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/books/:id" element={<BookDetails />} />
+          <Route path="/book/:id" element={<BookDetails />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </Router>
-    </div>
+      </div>
+    </Router>
   );
 }
