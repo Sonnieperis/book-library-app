c21ba5473378944940fa8c6dd3bace80fc554561
Muthoni(sonnieperis) <sonnieperis17@gmail.com>
2025-09-09 19:40:48 +0300

    initial commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,8 @@
-// src/pages/Home.jsx
-import React from "react";
-
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-4xl font-bold">Welcome to Book Library 📚</h1>
-      <p className="mt-4 text-lg">Browse books and discover details.</p>
+    <div className="p-8">
+      <h1 className="text-3xl font-bold">Welcome to the Book Library</h1>
+      <p className="mt-2">Start exploring books!</p>
     </div>
   );
 }
