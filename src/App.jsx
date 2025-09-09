1c89c0b18dff54ef6bd2e4fc4179e69a1e8ff3a0
Muthoni(sonnieperis) <sonnieperis17@gmail.com>
2025-09-09 18:26:54 +0300

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,16 @@ import NotFound from "./pages/NotFound";
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        {/* Home Page */}
-        <Route path="/" element={<Home />} />
+    <div className="min-h-screen bg-orange-900 text-white">
+      <Router>
+        <Routes>
+          {/* Home Page */}
+          <Route path="/" element={<Home />} />
 
-        {/* Catch-all route */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
+          {/* Catch-all route */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
