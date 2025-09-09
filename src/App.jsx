6715cf621ae2f9d3f2b8b472f704601526008582
Muthoni(sonnieperis) <sonnieperis17@gmail.com>
2025-09-09 17:38:12 +0300

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
+import Verification from "./pages/Verification";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/verify" element={<Verification />} />
       </Routes>
     </BrowserRouter>
   );
