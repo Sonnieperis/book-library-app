bbe38be38c13350c3c71b6e0cac62111e9fddf35
Muthoni(sonnieperis) <sonnieperis17@gmail.com>
2025-09-09 16:11:51 +0300

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import BookDetails from "./pages/BookDetails";
 import NotFound from "./pages/NotFound";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Home Page */}
         <Route path="/" element={<Home />} />
-
-        {/* Book Details Page */}
-        <Route path="/books/:id" element={<BookDetails />} />
-
-        {/* Fallback for undefined routes */}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
